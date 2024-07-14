d159ed5253f002a507940fe2aac1b66ce2f59360
Sandeep Jaiswal <sandeep.jaiswal@indiamart.com>
2024-07-15 01:58:28 +0530

    dashboard

---
+++ src/components/Dashboard.js
@@ -0,0 +1,65 @@
+import React, { useContext, useEffect, useState } from "react";
+import Overview from "./Overview";
+import Details from "./Details";
+import Chart from "./Chart";
+import Header from "./Header";
+import StockContext from "../context/StockContext";
+import { fetchStockDetails, fetchQuote } from "../utils/api/stock-api";
+
+const Dashboard = () => {
+  const { stockSymbol } = useContext(StockContext);
+
+  const [stockDetails, setStockDetails] = useState({});
+
+  const [quote, setQuote] = useState({});
+
+  useEffect(() => {
+    const updateStockDetails = async () => {
+      try {
+        const result = await fetchStockDetails(stockSymbol);
+        setStockDetails(result);
+      } catch (error) {
+        setStockDetails({});
+        console.log(error);
+      }
+    };
+
+    const updateStockOverview = async () => {
+      try {
+        const result = await fetchQuote(stockSymbol);
+        setQuote(result);
+      } catch (error) {
+        setQuote({});
+        console.log(error);
+      }
+    };
+
+    updateStockDetails();
+    updateStockOverview();
+  }, [stockSymbol]);
+
+  return (
+    <div>
+      <div className="col-span-1 md:col-span-2 xl:col-span-3 row-span-1 flex justify-start items-center">
+        <Header name={stockDetails.name} />
+      </div>
+      <div className="md:col-span-2 row-span-4">
+        <Chart />
+      </div>
+      <div>
+        <Overview
+          symbol={stockSymbol}
+          price={quote.pc}
+          change={quote.d}
+          changePercent={quote.dp}
+          currency={stockDetails.currency}
+        />
+      </div>
+      <div className="row-span-2 xl:row-span-3">
+        <Details details={stockDetails} />
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;

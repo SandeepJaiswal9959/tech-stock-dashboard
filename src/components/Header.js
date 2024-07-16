d6543515917e39f7fde8952d2a4d966f86f293bb
Sandeep Jaiswal <sandeep.jaiswal@indiamart.com>
2024-07-16 11:22:54 +0530

    updated changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { Component } from 'react'
 
-const Header = ({ name }) => {
-  return (
-    <>
-      <div className="xl:px-32">
-        <h1 className="text-5xl">{name}</h1>
-      </div>
-    </>
-  );
-};
+export class Header extends Component {
+    render() {
+        return (
+            <div>
+                <nav className="navbar navbar-expand-lg" style={{backgroundColor:"#fcdf03"}}>
+                    <div className="container-fluid">
 
-export default Header;
+                        <select className="form-select form-select-lg " aria-label=".form-select-lg example" name='selectCoin'
+                            style={{ width: "fit-content" }} onChange={this.props.handle_Submit}>
+                            <option value="bitcoin">Select Coin</option>
+                            <option value="binancecoin">Binance (BNB)</option>
+                            <option value="bitcoin">Bitcoin (BTC) </option>
+                            <option value="dogecoin">Dogecoin (DOGE)</option>
+                            <option value="ethereum">Ethereum (ETH)</option>
+                            <option value="solana">Solana (SOL)</option>
+                            <option value="tether">Tether (USDT)</option>
+                        </select>
+
+                        <a className="navbar-brand d-flex ml-auto display-2 text-dark fs-2 fw-bold text-uppercase "
+                            style={{ fontFamily: 'NHaasGroteskDSPro-65Md'}} href="/">Crypto 
+                            Dashboard</a>
+
+                    </div>
+                </nav>
+            </div>
+        )
+    }
+}
+
+export default Header
